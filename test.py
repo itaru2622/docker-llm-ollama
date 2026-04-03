81b13a1c82cd12b421889315727ffcc0fae4ebf6
itaru2622 <itaru2622@gmail.com>
2026-04-04 01:17:17 +0900

    update: test.py  support to run on python 3.14 by ignoring below warning message:
       UserWarning: Core Pydantic V1 functionality isn't compatible with Python 3.14 or greater.
       from pydantic.v1.fields import FieldInfo as FieldInfoV1

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+# starts: workaround ; add  code to ignore warning  """
+import warnings
+warnings.filterwarnings("ignore", message=".*Pydantic V1 functionality isn't compatible with.*")
+# ends: workaround ; add  code to ignore warning  """
+
 from langchain_ollama import ChatOllama
 
 model = ChatOllama(model="llama3.2")
-#result = model.invoke("...")
-#print(result.content)
+result = model.invoke("Hi, who is it?")
+print(result.content)
 
